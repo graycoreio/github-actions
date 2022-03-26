b271afaad7ff9078995d42d21ed8411d6760c048
Damien Retzinger <damienwebdev@gmail.com>
2022-03-26 15:39:57 -0400

    fix(angular-universal-vercel): working entrypoint

--- angular-universal-vercel/entrypoint.sh
+++ angular-universal-vercel/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ "$INPUT_PROD" = "true" ]; 
+echo "Copy workspace files to /app"
+cp -R "${GITHUB_WORKSPACE}/." "/app"
+
+echo "Switch current working directory to app"
+cd "/app"
+
+if [ "$INPUT_PROD" = "true" ]
+then
   echo "Deploy to Vercel (Prod)"
-  VERCEL_ORG_ID=$INPUT_VERCEL_ORG VERCEL_PROJECT_ID=$INPUT_PROJECT_ID npx vercel --token=$INPUT_VERCEL_TOKEN --prod
+  VERCEL_ORG_ID=$INPUT_VERCEL_ORG VERCEL_PROJECT_ID=$INPUT_VERCEL_PROJECT_ID npx vercel --token=$INPUT_VERCEL_TOKEN --prod
 else
   echo "Deploy to Vercel"
-  VERCEL_ORG_ID=$INPUT_VERCEL_ORG VERCEL_PROJECT_ID=$INPUT_PROJECT_ID npx vercel --token=$INPUT_VERCEL_TOKEN
-then
-
-
-
-
+  VERCEL_ORG_ID=$INPUT_VERCEL_ORG VERCEL_PROJECT_ID=$INPUT_VERCEL_PROJECT_ID npx vercel --token=$INPUT_VERCEL_TOKEN
+fi
