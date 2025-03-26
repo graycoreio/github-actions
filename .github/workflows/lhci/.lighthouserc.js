67456c5ff5669db8d28791b1b2d8ae02c850c11d
Damien Retzinger <damienwebdev@gmail.com>
2025-03-26 15:05:33 -0400

    add target

--- .github/workflows/lhci/.lighthouserc.js
+++ .github/workflows/lhci/.lighthouserc.js
@@ -1,7 +1,15 @@
+const domain = process.env.LHCI_COLLECT__URL;
+
 module.exports = {
     ci: {
         assert: {
             preset: "lighthouse:recommended",
         },
+        collect: {
+            url: [
+                domain + "/",
+                domain + "/test",
+            ]
+        }
     },
 };
